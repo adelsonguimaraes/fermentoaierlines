ceb217fc9f282e405f3aeb6486506b1968aec015
Adelson Guimarães Monteiro <adelsonguimaraes@gmail.com>
2019-07-24 22:31:39 -0400

    .

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -4,13 +4,28 @@ angular.module(module).controller('homeCtrl', function ($rootScope, $scope, auth
 
     $scope.title = 'Fermen.to Aierlines <i class="fa fa-plane"></i>';
 
+    $scope.obj = {
+        origem: 0,
+        destino: 0,
+        modalidade: 0
+    }
+
+    $scope.modalidades = [
+        { id: 0, descricao: "Selecione uma opção" },
+        { id: 1, descricao: "Ida e Volta" },
+        { id: 2, descricao: "Só Ida"}
+    ]
+
     $scope.destinos = [
+        { id: 0, nome: 'Selecione uma opção'},
         { id: 1, nome: "Manaus", brev: "MAO" },
         { id: 2, nome: "Rio de Janeiro", brev: "RJ" },
         { id: 3, nome: "São Paulo", brev: "SP" },
         { id: 4, nome: "Recife", brev: "REC" }
     ];
 
-    console.log($scope.destinos);
+    $scope.buscar = function (obj) {
+        console.log(obj);
+    }
     
 });
